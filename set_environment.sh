b4ab06717d704098795f5489f42fe058327ff2da
ninharp <sauer.uetersen@gmail.com>
2021-07-25 22:30:35 +0200

    Update set_environment.sh

--- set_environment.sh
+++ set_environment.sh
@@ -3,14 +3,14 @@
 ## Parameters configured here will override default and ENV values.
 
 ## Add your source directories here separated by space
-MODULES = app MD_Parola/src MD_MAX72XX/src
-EXTRA_INCDIR = include MD_Parola/src MD_MAX72XX/src
+export MODULES = app MD_Parola/src MD_MAX72XX/src
+export EXTRA_INCDIR = include MD_Parola/src MD_MAX72XX/src
 
 ## ESP_HOME sets the path where ESP tools and SDK are located.
-ESP_HOME = esp-open-sdk
+export ESP_HOME = esp-open-sdk
 
 ## SMING_HOME sets the path where Sming framework is located.
-SMING_HOME = Sming/Sming
+export SMING_HOME = Sming/Sming
 
 ## COM port parameter is reqruied to flash firmware correctly.
-COM_PORT = /dev/ttyUSB0
+export COM_PORT = /dev/ttyUSB0
